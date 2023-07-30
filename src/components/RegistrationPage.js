26646e0b39c18be4df83f733b790792c60c86922
Mohit Sahu <mohit.sahu2017@gmail.com>
2023-07-31 00:13:42 +0530

    mohit

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -13,7 +13,6 @@ const RegistrationPage = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [msg, setMsg] = useState("");
   const navigate = useNavigate();
-
   const passwordhandle= (event)=>{
     if(pass !== event.target.value){
       setMsg("Password Doesn't matched!");
@@ -26,7 +25,32 @@ const RegistrationPage = () => {
     }
     
   }
+  const handlemail = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value)
+    console.log(email)
+    if(email !==""){
+    BackendCall.getCall("get-user-by-id?userid="+email,"").then((response) =>{
+      if(response.data !==""){
+        setMsg("Mail id already exist");
+      }
+    }).catch(error =>{
+      console.log(error);
+    })}
+  }
   const handleRegister = () => {
+    if(firstName === ""){
+      setMsg("First Name can not be empty")
+    }else if(lastName === ""){
+      setMsg("Last Name can not be empty")
+    }else if(email === ""){
+      setMsg("Email Id can not be empty")
+    }else if(contactno === ""){
+      setMsg("Contact Number can not be empty")
+    }else if(pass === ""){
+      setMsg("Password can not be empty")
+    }
+    else{
     const user = {
       firstname : firstName,
       lastname : lastName,
@@ -36,8 +60,7 @@ const RegistrationPage = () => {
     }
 
     BackendCall.postCall("registration",user).then((response)=>{
-      console.log(response);
-      if(response.data === "successefully"){
+      if(response.data === "success"){
         setFirstName("");
         setLastName("");
         setEmail("");
@@ -45,16 +68,17 @@ const RegistrationPage = () => {
         setPassword("");
         navigate('/login');      
       }else{
-        alert("Somthing went wrong");
+        setMsg(response.data);
       }
     }).catch(error=>{
       console.log(error);
-    })
+    })}
   };
 
   return (
     <div className="registration-container">
       <h2>Registration Form</h2>
+      <h6 style={{color:'red'}}>{msg}</h6>
       <div className="setdisplay">
         <div className="firstset">
           <div className="form-group">
@@ -64,11 +88,13 @@ const RegistrationPage = () => {
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
               placeholder="Enter your First name"
-            />
+              required
+              />
           </div>
           <div className="form-group">
-            <label>Full Name:</label>
+            <label>Last Name:</label>
             <input
+            required
               type="text"
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
@@ -78,6 +104,7 @@ const RegistrationPage = () => {
           <div className="form-group">
             <label>Contact Number:</label>
             <input
+            required
               type="text"
               value={contactno}
               onChange={(e) => setContactNo(e.target.value)}
@@ -89,25 +116,28 @@ const RegistrationPage = () => {
           <div className="form-group">
             <label>Email:</label>
             <input
+            required
               type="text"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
+              onChange={handlemail}
               placeholder="Enter your email"
             />
           </div>
           <div className="form-group">
             <label>Password:</label>
             <input
+            required
               type="password"
               value={pass}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter your password"
             />
           </div>
-          <p>{msg}</p>
           <div className="form-group">
             <label>Confirm Password:</label>
             <input
+            required
               type="password"
               value={confirmPassword}
               // onChange={(e) => setConfirmPassword(e.target.value)}
